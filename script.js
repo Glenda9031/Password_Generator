ce5126336f893da38ccec02c9d69693629403bbc
Glenda Reed <glendareed0807@sbcglobal.net>
2023-06-03 19:12:04 -0500

    Update index.html. stylle.css and script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+const resultEl = do cument.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumver,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    const password = resultEl.innerText;
+    if (!password) {
+        return;
+    }
+
+    navigator.clipboard.writeText(password);
+    alert('Password copied to clipboard!')
+})
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+})
